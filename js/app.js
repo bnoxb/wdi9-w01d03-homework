5646912c0b86d3b334ead4e232a6f3e273f011e1
Robbie <saucy.cooking@gmail.com>
2018-12-11 20:37:41 -0700

    Commit 5 - Getting to know you answered

--- js/app.js
+++ js/app.js
@@ -26,3 +26,16 @@ for (i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+shahzad[0] = "Gameboy";
+daniel[1] += 1;
+ryan[2] = "Gotham City";
+reuben.splice(2, 1, "Chicago");
+jim.splice(2, 1, "Denver", "Tamarindo","London");
+jim.splice(2, 1);
